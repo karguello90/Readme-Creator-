9ac112ccc2342826ce2eb0d3fd34f3a02ed137cc
karguello90 <karguello90@gmail.com>
2023-01-12 13:58:36 -0500

    created init function to write readme file

--- index.js
+++ index.js
@@ -3,13 +3,28 @@ const generateMarkdown = require("./utils/generateMarkdown")
 const inquirer = require("inquirer")
 const fs = require("fs")
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+       type: "input", 
+       name: "title",
+       message: "Please enter name of application.",
+    }, 
+    {
+        type: "input",
+        name: "description",
+        message: "Please give a brief description of the application."
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then(response=>{
+        fs.writeFileSync("readme.md", generateMarkdown (response))
+    })
+}
 
 // Function call to initialize app
 init();
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -17,6 +17,7 @@ function generateMarkdown(data) {
 ## description
 ${data.description}
 
+##  
 `;
 }
 
